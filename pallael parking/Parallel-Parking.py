45eeaa4fa5102e218c4090be82f4369d9d4b0b45
yousef-alf <61252354+yousef-alf@users.noreply.github.com>
2020-10-21 18:29:27 +0300

    Update Parallel-Parking.py

--- pallael parking/Parallel-Parking.py
+++ pallael parking/Parallel-Parking.py
@@ -19,19 +19,17 @@ def control(pos_x, pos_y, time, velocity):
     
     # In this example the car will drive forward for three seconds
     # and then backs up until its pos_y is less than 32 then comes to a stop by braking
-
     if time < 3:
         car_parameters['throttle'] = 1
-    elif time < 6:
+    elif pos_y > 40:
         car_parameters['throttle'] = -1
-    elif time < 9:
+    elif pos_y > 35:
         car_parameters['steer'] = 1
-    elif time < 11.5:
+    elif pos_y > 32:
         car_parameters['steer'] = -1
     else:
         car_parameters['throttle'] = 0
         car_parameters['brake'] = 1
-        car_parameters['steer'] = 0
     
     return car_parameters
     
